48b9177ed6804ca5ec7da3360b6881bfceb574ef
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-17 14:50:34 +0200

    Add UserList React component in script.js

--- script.js
+++ script.js
@@ -36,6 +36,21 @@ class App extends React.Component {
         );
     }
 }
+
+class UsersList extends React.Component {
+    get users() {
+        return this.props.users.map(user => <User key={user.id} user={user} />);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.users}
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
     <App />,
     document.getElementById('root')
